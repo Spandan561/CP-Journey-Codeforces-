1b14c3f403cce5eaaed18deaf33edac5587300d3
Spandan Basu <71368455+Spandan561@users.noreply.github.com>
2022-06-09 19:07:31 +0600

    Create A. Print a Pedestal.cpp

---
+++ Codeforces Round #797 (Div. 3)/A. Print a Pedestal.cpp
@@ -0,0 +1,104 @@
+/**THE FUTURE IS UNPREDICTABLE. SO, BE REMEMBERED THROUGH THE LOVE YOU GIVE & GET. NOT FORGOTTEN FOR HATE OR JUDGEMENT**/
+/**Man can do everything,if he is determined. Cause man is not made for defeat, man can be destroyed but not defeated**/
+
+/****************************************
+**     Solution by Baspannissu         **
+****************************************/
+
+#include<bits/stdc++.h>
+#include<stdio.h>
+#include <algorithm>
+#include <string.h>
+
+#define  ll         long long
+#define  llu        unsigned long long
+
+#define  pi         ((2.0)*acos(0.0))
+#define  pb         push_back
+#define  pf         push_front
+#define  eb         emplace_back
+#define  mp         make_pair
+#define  all(x)     (x).begin(),(x).end()
+#define  X          first
+#define  Y          second
+
+/*loop*/
+#define  spa(i,p,n,m)   for(ll i=n;i<m;i+=p)
+#define  den(i,n,m)     for(int i=n;i>=m;i--)
+#define  bas(i,p,n,m)   for(ll i=n;i<=m;i+=p)
+
+/*output shortcuts*/
+#define  nl         "\n"
+#define  Yes        cout<<"Yes\n"
+#define  yes        cout<<"yes\n"
+#define  YES        cout<<"YES\n"
+#define  No         cout<<"No\n"
+#define  no         cout<<"no\n"
+#define  NO         cout<<"NO\n"
+#define take(n)     scanf("%d",&n)
+#define give(n)     printf("%d\n",n)
+
+#define  fastio     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+using namespace std;
+
+
+
+ll ceill(ll x,ll y)
+{
+    if(x%y==0)
+    {
+        return x/y;
+    }
+    return (x/y)+1;
+}
+
+ll increase(ll x)
+{
+    x++;
+    while(x%10==0)
+
+    {
+        x/=10;
+    }
+    return x;
+}
+
+void body()
+{
+    ll n; cin>>n;
+
+    ll t = (n-3)/3;
+    ll res = n-t;
+    ll f, s;
+    if(res%2==0)
+    {
+        f = (res+2)/2;
+        s = n-(t+f);
+    }
+    else
+    {
+        s = (res-1)/2;
+        f = (res+1)/2;
+    }
+    cout<<s<<" "<<f<<" "<<t<<nl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;cin>>t;
+    //body();
+
+    while(t--)
+    {
+        body();
+    }
+    return 0;
+
+
+}
+
+
