1c5c9211d8b4e63b92f55b1fc87200f559edd688
Spandan Basu <71368455+Spandan561@users.noreply.github.com>
2022-06-11 17:30:42 +0600

    Create A. Hexadecimal's theorem.cpp

---
+++ Random Solve/A. Hexadecimal's theorem.cpp
@@ -0,0 +1,93 @@
+/**THE FUTURE IS UNPREDICTABLE. SO, BE REMEMBERED THROUGH THE LOVE YOU GIVE & GET. NOT FORGOTTEN FOR HATE OR JUDGEMENT**/
+/**Man can do everything,if he is determined. Cause man is not made for defeat, man can be destroyed but not defeated**/
+
+/****************************************
+**     Solution by Baspannissu         **
+****************************************/
+
+#include<bits/stdc++.h>
+#include<stdio.h>
+#include <algorithm>
+#include <string.h>
+
+#define  ll         long long
+#define  llu        unsigned long long
+
+#define  pi         ((2.0)*acos(0.0))
+#define  pb         push_back
+#define  pf         push_front
+#define  eb         emplace_back
+#define  mp         make_pair
+#define  all(x)     (x).begin(),(x).end()
+#define  X          first
+#define  Y          second
+
+/*loop*/
+#define  spa(i,p,n,m)   for(ll i=n;i<m;i+=p)
+#define  den(i,n,m)     for(int i=n;i>=m;i--)
+#define  bas(i,p,n,m)   for(ll i=n;i<=m;i+=p)
+
+/*output shortcuts*/
+#define  nl         "\n"
+#define  Yes        cout<<"Yes\n"
+#define  yes        cout<<"yes\n"
+#define  YES        cout<<"YES\n"
+#define  No         cout<<"No\n"
+#define  no         cout<<"no\n"
+#define  NO         cout<<"NO\n"
+#define take(n)     scanf("%d",&n)
+#define give(n)     printf("%d\n",n)
+
+#define  fastio     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+using namespace std;
+ll fib[50];
+void fibo_make()
+{
+    ll x=0;ll y=1;
+    fib[0]=x;fib[1]=y;
+    for(ll i=2;i<50;i++)
+    {
+        fib[i]=fib[i-1]+fib[i-2];
+    }
+}
+
+void body()
+{
+    fibo_make();
+    int n; cin>>n;
+    if(n==1)
+    {
+        cout<<"0 0 1\n";
+        return;
+    }
+    for(ll i=0;i<50;i++)
+    {
+        if(fib[i]==n)
+        {
+            cout<<0<<" "<<fib[i-2]<<" "<<fib[i-1]<<nl;
+            return ;
+        }
+    }
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    //ll t;cin>>t;
+    body();
+/*
+    while(t--)
+    {
+        body();
+    }*/
+    return 0;
+
+
+
+}
+
+ 
