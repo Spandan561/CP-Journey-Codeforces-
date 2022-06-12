75f077883f8fd28afc903bc869d898ed14a3215b
Spandan Basu <71368455+Spandan561@users.noreply.github.com>
2022-06-12 20:20:27 +0600

    Create  A. Circular Local MiniMax.cpp

---
+++ Random Solve/A. Circular Local MiniMax.cpp
@@ -0,0 +1,130 @@
+/**THE FUTURE IS UNPREDICTABLE. SO, BE REMEMBERED THROUGH THE LOVE YOU GIVE & GET. NOT FORGOTTEN FOR HATE OR JUDGEMENT**/
+/**Man can do everything,if he is determined. Cause man is not made for defeat, man can be destroyed but not defeated**/
+
+/****************************************
+**     Solution by Baspannissu         **
+****************************************/
+
+#include<bits/stdc++.h>
+#include<stdio.h>
+#include <algorithm>
+#include <string.h>
+
+#define  ll         long long
+#define  llu        unsigned long long
+
+#define  pi         ((2.0)*acos(0.0))
+#define  pb         push_back
+#define  pf         push_front
+#define  eb         emplace_back
+#define  mp         make_pair
+#define  all(x)     (x).begin(),(x).end()
+#define  X          first
+#define  Y          second
+
+/*loop*/
+#define  spa(i,p,n,m)   for(ll i=n;i<m;i+=p)
+#define  den(i,n,m)     for(int i=n;i>=m;i--)
+#define  bas(i,p,n,m)   for(ll i=n;i<=m;i+=p)
+
+/*output shortcuts*/
+#define  nl         "\n"
+#define  Yes        cout<<"Yes\n"
+#define  yes        cout<<"yes\n"
+#define  YES        cout<<"YES\n"
+#define  No         cout<<"No\n"
+#define  no         cout<<"no\n"
+#define  NO         cout<<"NO\n"
+#define take(n)     scanf("%d",&n)
+#define give(n)     printf("%d\n",n)
+
+#define  fastio     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+///sorting, greedy ,implementation
+using namespace std;
+ll fib[50];
+void fibo_make()
+{
+    ll x=0;ll y=1;
+    fib[0]=x;fib[1]=y;
+    for(ll i=2;i<50;i++)
+    {
+        fib[i]=fib[i-1]+fib[i-2];
+    }
+}
+
+void body()
+{
+    ll n; cin>>n;
+    ll a[n+3];
+
+    for(ll i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    sort(a,a+n);
+    if(n%2==1)
+    {
+        NO;
+        return;
+    }
+    vector<ll>v;
+    for(ll i=0,j=n/2;i<n/2,j<n;i++,j++)
+    {
+        v.pb(a[i]);
+        v.pb(a[j]);
+    }
+    v.pb(v[0]);
+    v.pb(v[1]);
+    bool flag=true;
+
+    for(ll i=1;i<v.size()-1;i++)
+    {
+        if(v[i]>v[i-1]&& v[i]>v[i+1])
+        {
+            continue;
+        }
+        else if(v[i]<v[i-1]&& v[i]<v[i+1])
+        {
+            continue;
+        }
+        else
+        {
+            flag=false;
+            break;
+        }
+    }
+    if(flag==false)
+    {
+        NO;
+    }
+    else
+    {
+        YES;
+        for(ll i=0;i<n;i++)
+        {
+            cout<<v[i]<<" ";
+        }
+        cout<<nl;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;cin>>t;
+    //body();
+
+    while(t--)
+    {
+        body();
+    }
+    return 0;
+
+
+
+}
+
+
