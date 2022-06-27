74011d705183cf8b021f3f2025d61d4986f33a36
Spandan Basu <71368455+Spandan561@users.noreply.github.com>
2022-06-27 18:40:54 +0600

    Create A.NIT orz!.cpp

---
+++ Codeforces Global Round 21/A.NIT orz!.cpp
@@ -0,0 +1,144 @@
+/**THE FUTURE IS UNPREDICTABLE. SO, BE REMEMBERED THROUGH THE LOVE YOU GIVE & GET. NOT FORGOTTEN FOR HATE OR JUDGEMENT**/
+/**Man can do everything,if he is determined. Cause man is not made for defeat, man can be destroyed but not defeated**/
+
+/****************************************
+**     Solution by Baspannissu         **
+****************************************/
+
+#include<bits/stdc++.h>
+#include<stdio.h>
+#include <algorithm>
+#include <string.h>
+
+#define  ll         long long
+#define  llu        unsigned long long
+
+#define  pi         ((2.0)*acos(0.0))
+#define  pb         push_back
+#define  pf         push_front
+#define  eb         emplace_back
+#define  mp         make_pair
+#define  all(x)     (x).begin(),(x).end()
+#define  X          first
+#define  Y          second
+
+/*loop*/
+#define  spa(i,p,n,m)   for(ll i=n;i<m;i+=p)
+#define  den(i,n,m)     for(int i=n;i>=m;i--)
+#define  bas(i,p,n,m)   for(ll i=n;i<=m;i+=p)
+
+/*output shortcuts*/
+#define  nl         "\n"
+#define  Yes        cout<<"Yes\n"
+#define  yes        cout<<"yes\n"
+#define  YES        cout<<"YES\n"
+#define  No         cout<<"No\n"
+#define  no         cout<<"no\n"
+#define  NO         cout<<"NO\n"
+#define take(n)     scanf("%d",&n)
+#define give(n)     printf("%d\n",n)
+
+#define  fastio     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+using namespace std;
+ll fib[50];
+void fibo_make()
+{
+
+    ll x=0;ll y=1;
+    fib[0]=x;fib[1]=y;
+    for(ll i=2;i<50;i++)
+    {
+        fib[i]=fib[i-1]+fib[i-2];
+    }
+}
+
+ll mod(ll b,ll p, ll m)
+{
+    if(p==0)
+    {
+        return 1;
+    }
+    else if(p%2==0)
+    {
+        ll x = (mod(b,p/2,m)%m);
+        return (x*x)%m;
+    }
+    else if(p%2==1)
+    {
+        ll x = b;
+        ll y = (mod(b,p-1,m)%m);
+        return (x*y)%m;
+    }
+}
+
+void body()
+{
+    ll n,k; cin>>n>>k;
+    ll a[n+3];
+    vector<ll>v1;
+
+    for(ll i=0;i<n;i++)
+    {
+        cin>>a[i];
+        //ll x= (a[i] | k);
+        //cout<<x<<nl;
+        v1.pb(a[i]|k);
+    }
+
+
+    sort(a,a+n); sort(v1.begin(),v1.end());
+
+
+    cout<<max(a[n-1],v1[n-1])<<nl;
+    /*
+    ll j=0,mx=0,p=INT_MAX,x;
+
+    while(1)
+    {
+        if(k==0 || k==x)
+        {
+            cout<<mx<<nl;
+            break;
+        }
+        p=a[j];
+        a[j]=a[j]|k;
+        mx = max(mx,a[j]);
+        x=p&k;
+        k=min(x,k);
+
+        if(j==n)
+        {
+            j=0;
+            //k=p;
+        }
+        else
+        {
+            j++;
+        }
+        //cout<<p<<nl;
+    }
+    */
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;cin>>t;
+    //body();
+
+
+    while(t--)
+    {
+        body();
+    }
+    return 0;
+
+
+
+}
+
+
