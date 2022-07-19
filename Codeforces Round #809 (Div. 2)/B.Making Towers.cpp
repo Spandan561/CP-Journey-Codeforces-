9fa8ef37dd7c72d8106c3aaec046408c6b68ed88
Spandan Basu <71368455+Spandan561@users.noreply.github.com>
2022-07-19 20:20:37 +0600

    Create B.Making Towers.cpp

---
+++ Codeforces Round #809 (Div. 2)/B.Making Towers.cpp
@@ -0,0 +1,132 @@
+/**THE FUTURE IS UNPREDICTABLE. SO, BE REMEMBERED THROUGH THE LOVE YOU GIVE & GET. NOT FORGOTTEN FOR HATE OR JUDGEMENT**/
+/**Man can do everything,if he is determined. Cause man is not made for defeat, man can be destroyed but not defeated**/
+
+/****************************************
+**     Solution by Baspannissu         **
+****************************************/
+
+#include<bits/stdc++.h>
+#include<stdio.h>
+#include <algorithm>
+#include <string.h>
+
+#define  ll         long long
+#define  llu        unsigned long long
+
+#define  pi         ((2.0)*acos(0.0))
+#define  pb         push_back
+#define  pf         push_front
+#define  eb         emplace_back
+#define  mp         make_pair
+#define  all(x)     (x).begin(),(x).end()
+#define  X          first
+#define  Y          second
+
+/*loop*/
+#define  spa(i,p,n,m)   for(ll i=n;i<m;i+=p)
+#define  den(i,n,m)     for(int i=n;i>=m;i--)
+#define  bas(i,p,n,m)   for(ll i=n;i<=m;i+=p)
+
+/*output shortcuts*/
+#define  nl         "\n"
+#define  Yes        cout<<"Yes\n"
+#define  yes        cout<<"yes\n"
+#define  YES        cout<<"YES\n"
+#define  No         cout<<"No\n"
+#define  no         cout<<"no\n"
+#define  NO         cout<<"NO\n"
+#define take(n)     scanf("%d",&n)
+#define give(n)     printf("%d\n",n)
+
+#define  fastio     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+using namespace std;
+ll fib[50];
+void fibo_make()
+{
+
+    ll x=0;ll y=1;
+    fib[0]=x;fib[1]=y;
+    for(ll i=2;i<50;i++)
+    {
+        fib[i]=fib[i-1]+fib[i-2];
+    }
+}
+
+ll mod(ll b,ll p, ll m)
+{
+    if(p==0)
+    {
+        return 1;
+    }
+    else if(p%2==0)
+    {
+        ll x = (mod(b,p/2,m)%m);
+        return (x*x)%m;
+    }
+    else if(p%2==1)
+    {
+        ll x = b;
+        ll y = (mod(b,p-1,m)%m);
+        return (x*y)%m;
+    }
+}
+
+void body()
+{
+    ll n; cin>>n;
+    vector<ll>v[n+2];
+
+    for(ll i=1;i<=n;i++)
+    {
+        ll x; cin>>x;
+        v[x].pb(i);
+    }
+    for(ll i=1;i<=n;i++)
+    {
+        ll cnt=1;
+        if(v[i].size()<=1)
+        {
+            cout<<v[i].size()<<" ";
+        }
+        else
+        {
+            for(ll j=1;j<v[i].size();j++)
+            {
+                ll p = v[i][j]-v[i][j-1]-1;
+                //p--;
+                if(p%2==0)
+                {
+                    cnt++;
+                }
+            }
+            cout<<cnt<<" ";
+        }
+        //cout<<nl;
+    }
+    cout<<nl;
+
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;cin>>t;
+    //body();
+
+
+    while(t--)
+    {
+        body();
+    }
+
+    return 0;
+
+
+
+}
+
+
