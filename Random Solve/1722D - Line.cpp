84d942e68e2683791c8f39c300e6ba361427f7f5
Spandan Basu <71368455+Spandan561@users.noreply.github.com>
2022-09-28 00:53:59 +0600

    Create 1722D - Line.cpp

---
+++ Random Solve/1722D - Line.cpp
@@ -0,0 +1,164 @@
+#include<bits/stdc++.h>
+#include<stdio.h>
+#include <algorithm>
+#include <string.h>
+
+#define  ll         long long
+#define  llu        unsigned long long
+
+#define  pi         ((2.0)*acos(0.0))
+#define  pb         push_back
+#define  pf         push_front
+#define  eb         emplace_back
+#define  mp         make_pair
+#define  all(x)     (x).begin(),(x).end()
+#define  X          first
+#define  Y          second
+
+/*loop*/
+#define  spa(i,p,n,m)   for(ll i=n;i<m;i+=p)
+#define  den(i,n,m)     for(int i=n;i>=m;i--)
+#define  bas(i,p,n,m)   for(ll i=n;i<=m;i+=p)
+
+/*output shortcuts*/
+#define  nl         "\n"
+#define  Yes        cout<<"Yes\n"
+#define  yes        cout<<"yes\n"
+#define  YES        cout<<"YES\n"
+#define  No         cout<<"No\n"
+#define  no         cout<<"no\n"
+#define  NO         cout<<"NO\n"
+#define take(n)     scanf("%d",&n)
+#define give(n)     printf("%d\n",n)
+
+#define  fastio     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+using namespace std;
+ll fib[50];
+void fibo_make()
+{
+
+    ll x=0;ll y=1;
+    fib[0]=x;fib[1]=y;
+    for(ll i=2;i<50;i++)
+    {
+        fib[i]=fib[i-1]+fib[i-2];
+    }
+}
+
+ll mod(ll b,ll p, ll m)
+{
+    if(p==0)
+    {
+        return 1;
+    }
+    else if(p%2==0)
+    {
+        ll x = (mod(b,p/2,m)%m);
+        return (x*x)%m;
+    }
+    else if(p%2==1)
+    {
+        ll x = b;
+        ll y = (mod(b,p-1,m)%m);
+        return (x*y)%m;
+    }
+}
+int sqr(int x)
+{
+    if(x==0 || x==1)
+    {
+        return x;
+    }
+    int cnt=1, ans=1;
+    while(ans<=x)
+    {
+        cnt++;
+        ans = cnt*cnt;
+    }
+    return cnt-1;
+}
+ll x[1000007];
+void seive()
+{
+
+    x[0]=x[1]=0;
+    ll n=1000007;
+    //int s= sqrt(n);
+    for(int i=2;i<=n;i++)
+    {
+        x[i]=1;
+    }
+    int s=sqrt(n);
+    for(int i=2;i<=s;i++)
+    {
+        if(x[i]==1)
+        {
+            for(int j=i*i;j<=n;j+=i)
+            {
+                x[j]=0;
+            }
+        }
+    }
+
+}
+ll add(ll x,ll y,ll m)
+{
+    x+=y;
+    return (x>=m) ? x-m:x;
+}
+void body()
+{
+    ll n; cin>>n;
+    string s; cin>>s;
+    ll cnt=0;
+
+    vector<ll>v1;
+
+    for(ll i=0;i<n;i++)
+    {
+        if(s[i]=='L')
+        {
+            v1.pb((n-i-1)-i);
+            cnt+=i;
+        }
+        else
+        {
+            v1.pb(i-(n-i-1));
+            cnt+=(n-i-1);
+        }
+    }
+    sort(v1.rbegin(),v1.rend());
+    for(ll i=0;i<n;i++)
+    {
+        if(v1[i]>0)
+        {
+            cnt+=v1[i];
+        }
+        cout<<cnt<<" ";
+    }
+    cout<<nl;
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t; cin>>t;
+    //body();
+
+
+    while(t--)
+    {
+        body();
+    }
+
+    return 0;
+
+
+
+}
+
+
